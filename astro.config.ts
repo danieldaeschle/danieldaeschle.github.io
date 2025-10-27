5f1f122352963d2351608e2df54ca4f065c45698
Daniel Däschle <daniel.daeschle@gmail.com>
2025-10-27 20:20:53 +0100

    feat: add Astro configuration and lucide-astro import optimizer plugin

---
+++ astro.config.ts
@@ -0,0 +1,12 @@
+import { defineConfig } from "astro/config";
+import tailwindcss from "@tailwindcss/vite";
+
+import lucideAstroImportOptimizer from "./lucide.config";
+
+// https://astro.build/config
+export default defineConfig({
+  site: "https://danieldaeschle.github.io",
+  vite: {
+    plugins: [tailwindcss(), lucideAstroImportOptimizer()],
+  },
+});
